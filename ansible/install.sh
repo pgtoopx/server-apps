2772f54bcaf6f45f047410922f3e3bfbca2053d0
pgtoopx <eduardoinbrand@gmail.com>
2023-08-02 12:01:44 -0400

    fix: se actualiza url playbook

--- ansible/install.sh
+++ ansible/install.sh
@@ -9,7 +9,7 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
 # Descargar el playbook de Ansible
-# curl -O https://ruta_del_archivo/playbook.yml
+curl -O https://raw.githubusercontent.com/pgtoopx/server-apps/main/ansible/playbook.yaml
 
 # Ejecutar el playbook de Ansible
 ansible-playbook -i "localhost," -c local playbook.yml
